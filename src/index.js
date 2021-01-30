29f89fa73c7efea74efb1eb0274e9447c97d037d
Oleg Soldatenko <sunskays@gmail.com>
2021-01-30 15:24:40 +0200

    Ver 1.8 - Функция-Scroll

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import './styles.css';
 
 // ----------------------------> Создали шабоны разметки и импортировали их
-import cardTemplete from './template/card.hbs';
+
 import listTemplete from './template/list.hbs';
 
 // ----------------------------> Импортируем плагин уведомлений - PNotify
 import { notice, success, info, error, defaultModules} from '../node_modules/@pnotify/core/dist/PNotify.js';
-import "@pnotify/core/dist/PNotify.css";                     //---> Импортирует стилей
-import '@pnotify/core/dist/BrightTheme.css';                 //---> Импортирует цветовую гамму
-import 'material-design-icons/iconfont/material-icons.css'; //---> Импортирует иконки
+import "@pnotify/core/dist/PNotify.css";                     //---> Импортирт стилей
+import '@pnotify/core/dist/BrightTheme.css';                 //---> Импортирт цветовой гаммы
+import 'material-design-icons/iconfont/material-icons.css'; //---> Импортирт иконок
 
 // ----------------------------> Изменили дефолтные настройки уведомлений (PNotify)
 const PNotify = {
@@ -18,97 +18,69 @@ const PNotify = {
     closerHover: false,   //---> Видно иконку закрытия
 }
 
-// ----------------------------> Импортировали ф-ю debounce для задержки
-var debounce = require('lodash.debounce');
-
-// ---------------------------------------------------------------------
-
-console.log('Hello');
+// ----------------------------> Общие данные:
 
 const API = {
     key: '20057586-194248245024f7fdf6233c620',
     page: 1,
     onPage: 12,
+    search: '',
 }
-const { key, onPage } = API;
 
-let page = 1;
-let search;
+const REF = {
+    form: document.querySelector('#search-form'),
+    input: document.querySelector('#name-input'),
+    btn: document.querySelector('button[data-search="search"]'),
+    box: document.querySelector('#js-search'),
+}
 
-const formRef = document.querySelector('#search-form');
-const input = document.querySelector('#name-input');
-const btnRef = document.querySelector('button[data-search="search"]');
-const boxRef = document.querySelector('#js-search');
+// ----------------------------> Ф-я поиска:
 
 function fnSubmit(event) {
-    if (input.value.length > 0) {
+    if (REF.input.value.length > 0) {
         event.preventDefault();
-        search = input.value;
-        // const form = event.currentTarget;
-        // form.reset();
-        boxRef.innerHTML = '';
-        page = 1;
-        fnFetchServer(search);
+        API.search = REF.input.value;
+        // const form = event.currentTarget // form.reset();
+        REF.box.innerHTML = '';
+        API.page = 1;
+        fnFetchServer(API.search);
     }
     return;
 }
-// 111
 
-formRef.addEventListener('submit', fnSubmit);
+// ----------------------------> Ф-я дозагрузки:
 
-function fnFetchServer(search) {
-    const url = `https://pixabay.com/api/?image_type=photo&key=${key}&page=${page}&per_page=${onPage}&orientation=horizontal&q=${search}`;
+function fnClick() {
+    API.page = API.page + 1;
+    fnFetchServer(API.search);
+}
+
+// ----------------------------> События:
+
+REF.form.addEventListener('submit', fnSubmit);
+REF.btn.addEventListener('click', fnClick); 
+
+// ----------------------------> Ф-я запроса на сервер:
+
+function fnFetchServer() {
+    const url = `https://pixabay.com/api/?image_type=photo&key=${API.key}&page=${API.page}&per_page=${API.onPage}&orientation=horizontal&q=${API.search}`;
         return fetch(url)
             .then(responce => responce.json())
             .then(data => fnTemplate(data))
             .catch(() => console.warn('Ошибка связи с сервером'));
 };
 
+// ----------------------------> Ф-я создания DOM-дерева:
 function fnTemplate(data) {
     const template = listTemplete(data.hits);
-    boxRef.insertAdjacentHTML('beforeend', template);
-    btnRef.classList.remove("hiden");
+    REF.box.insertAdjacentHTML('beforeend', template);
+    REF.btn.classList.remove("hiden");
+    window.scrollTo({
+        top: document.documentElement.offsetHeight,
+        left: 100,
+        behavior: 'smooth'
+    });
 }
-
-
-function fnClick() {
-    page = page + 1;
-    fnFetchServer(search);
-}
-
-btnRef.addEventListener('click', fnClick); 
-
-
-
-
-// function fnTemplate(data) {
-//     // console.log(data);
-//     // console.log(data.hits);
-//     const template = listTemplete(data.hits);
-//     console.log(input.value);
-
-//     // boxRef.innerHTML = '';
-//     boxRef.insertAdjacentHTML('beforeend', template);
-//     btnRef.classList.remove("hiden");
-//     page = page + 1;
-//     // console.log(search);
-//     if (searchValue !== input.value) {
-//         console.log('hi');
-//     }
-//     return;
-// }
-
-// const fnClick = () => {
-//     page = page + 1;
-//     fnFetch();
-// }
-    
-// const fnDelete = () => {
-//     page = page;
-//     fnFetch();
-//     }
-
-
 
 
 // const fnTemplate = (data) => {
